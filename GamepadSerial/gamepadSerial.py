d930fa323debc2574d6c1979173fed74227d3e34
Diego AW <diegoalmendrow@gmail.com>
2024-03-22 19:47:10 +0100

    espnova

--- GamepadSerial/gamepadSerial.py
+++ GamepadSerial/gamepadSerial.py
@@ -1,5 +1,6 @@
 import inputs
 import serial
+
 #import string
 
 def read_gamepad():
@@ -14,9 +15,21 @@ def read_gamepad():
             for event in events:
                 # Convert the event data to a string
                 if event.code != 'SYN_REPORT':
-                    data = f"{event.code} {event.state}\0"  # Add a newline character between each event's data, importante el \0
+                    key = event.code
+                    state = int(event.state)
+                    code = {'ABS_Z': 'LT', 'ABS_RZ': 'RT', 
+                            'BTN_TL': 'LB', 'BTN_TR': 'RB', 
+                            'ABS_HAT0X': 'GX', 'ABS_HAT0Y': 'GY',
+                            'BTN_START': 'LC', 'BTN_SELECT': 'RC',
+                            'BTN_WEST': 'BX', 'BTN_NORTH': 'BY',
+                            'BTN_SOUTH': 'BA', 'BTN_EAST': 'BB',
+                            'ABS_X': 'LX', 'ABS_Y': 'LY',
+                            'ABS_RX': 'RX', 'ABS_RY': 'RY',
+                            'BTN_THUMBL': 'LJ', 'BTN_THUMBR': 'RJ'}
+                    data = f"{code[key]}"
                     ser.write(data.encode())
-                    print(data)
+                    ser.write(state.to_bytes(2, byteorder='big', signed=True))
+                    print(data, state)
         else:
             print("No gamepad found.")
 
